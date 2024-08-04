20ef32e93cd9960c5ffa2ffeeab32c520be412b0
Umakaran Nemallan <unemallan@gmail.com>
2024-08-04 12:43:28 -0700

    First Working Version of Pulling Git Profile

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
- var express = require('express');
- var path = require('path');
+var express = require('express');
+var path = require('path');
+const github = require ('github-profile');
+var routes = require('./routes/index');
+ 
+// Create a new Express application.
+var app = express();
+ 
+// Configure view engine to render EJS templates.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+ 
+// Use application-level middleware for common functionality, including logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/views')));
+ 
 
- var routes = require('./routes/index');
+app.use('/', routes);
  
- // Create a new Express application.
- var app = express();
- 
- // Configure view engine to render EJS templates.
- app.set('views', __dirname + '/views');
- app.set('view engine', 'ejs');
- 
- // Use application-level middleware for common functionality, including logging, parsing, and session handling.
- app.use(require('morgan')('combined'));
- app.use(require('cookie-parser')());
- app.use(require('body-parser').urlencoded({ extended: true }));
- app.use(express.static(path.join(__dirname, '/views')));
- 
+app.post('/getinfo', (req,res) =>{
+    github(req.body.email).then((profile)=>{
+        console.log(profile);
+        res.render('index', {profile:profile});
+    });
+});
 
- app.use('/', routes);
- 
- const port_runing = 3000;
 
- app.listen(port_runing);
+const port_runing = 3000;
 
- console.log("Application running at: " + port_runing)
+app.listen(port_runing);
+
+console.log("Application running at: " + port_runing)
