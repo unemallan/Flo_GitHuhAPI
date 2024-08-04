75345030dd6b58949e6a76f60ffea997904db9a5
Umakaran Nemallan <unemallan@gmail.com>
2024-08-04 10:56:57 -0700

    Using Standard EJS template

--- index.js
+++
@@ -1,5 +0,0 @@
-const github = require ('github-profile')
-github('unemallan@gmail.com')
-    .then((profile)=>
-    {console.log(profile)
-})
--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Installing Needed NPM Packages"
-
-npm init -y
-npm i octokit
-npm i github-profile
-npm i express ejs
